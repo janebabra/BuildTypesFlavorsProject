f366ffbbc2978f4ae5bba5adb1c95e6eb8f1fc92
Barbara Hohensee <mrmac@mr-macs-macbook.local>
2013-10-21 10:28:18 +0200

    Build Flavors Project for Android Studio

---
+++ BuildTypesFlavors/src/main/java/com/greendog/buildtypesflavors/MainActivity.java
@@ -0,0 +1,39 @@
+package com.greendog.buildtypesflavors;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    WebView wv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wv = (WebView)findViewById(R.id.server_url_text_view);
+
+        wv.getSettings().setJavaScriptEnabled(false);
+        wv.getSettings().setBuiltInZoomControls(true);
+        wv.getSettings().setLoadWithOverviewMode(true);
+        wv.getSettings().setUseWideViewPort(true);
+
+        wv.loadUrl(Constants.BASE_URL);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+}
+
+
+
---
+++ BuildTypesFlavors/src/testing/java/com/greendog/buildtypesflavors/Constants.java
@@ -0,0 +1,5 @@
+package com.greendog.buildtypesflavors;
+
+public class Constants {
+    public final static String BASE_URL = "https://github.com/janebabra/MySwipeImageViewerProject/blob/master/README.md";
+}
